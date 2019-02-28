3a411aec786fd2882ba60470099df53a4df2fee5
circuscode <marco.hitschler@spelzenhof.com>
2019-02-28 23:22:46 +0100

    Bugfix CC Link

--- inc/cc.php
+++ inc/cc.php
@@ -7,24 +7,24 @@
  * @since 0.6
  */
 
-function huhu_creative_commons_logo() {
+function huhu_creative_commons_logo($id) {
 
 	$huhu_theme_url=get_template_directory_uri();
 	
-	echo '<a href="'.huhu_creative_commons_link().'">';
+	echo '<a href="'.huhu_creative_commons_link($id).'">';
 	echo '<img src="'.$huhu_theme_url.'/assets/creativecommons.png" width="500" height="119" alt="Creative Commons"/>';
 	echo '</a>';
 	
 }
 
-function huhu_creative_commons_link() {
+function huhu_creative_commons_link($id) {
 
-	$this_version=null;
+	$cc_version=null;
 
 	if (function_exists('get_field')) { 
-		$this_version = get_field( "licence_version");
+		$cc_version = get_field( "licence_version");
 	} else { 
-		$this_version = get_post_meta( get_the_ID(), 'licence_version');
+		$cc_version = get_post_meta( $id, 'licence_version', true);
 	} 
 	 
 	if ($cc_version=='4') {
